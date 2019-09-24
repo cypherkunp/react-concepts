6e967536da7e1860c1fec08a36245454e78d9ae8
Devvrat Shukla <devvratshukla@gmail.com>
2019-09-24 16:07:17 +0530

    feat(quiz): Added solution to xor component quiz

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/home-page/home-page.component';
 import ErrorBoundaryPage from './pages/error-boundary-page/error-boundary-page.component';
 import ControlledComponentPage from './pages/controlled-component-page/controlled-component-page.component';
+import XORComponent from './quiz-components/xor-checkbox/xor-checkbox.component';
 
 function App() {
     return (
@@ -13,9 +14,10 @@ function App() {
             <header className="app-header">All Things React!</header>
             <main className="app-container">
                 <Switch>
-                    //QUIZ ROUTES
-                    <Route path="/quiz/xor-checkbox" component={null} />
-                    //CONCEPTS ROUTES
+                    {/*QUIZ ROUTES*/}
+                    <Route exact path="/quiz/xor-checkbox" component={XORComponent} />
+
+                    {/*CONCEPTS ROUTES*/}
                     <Route path="/controlled-component" component={ControlledComponentPage} />
                     <Route path="/error-boundary" component={ErrorBoundaryPage} />
                     <Route exact path="/" component={HomePage} />
---
+++ src/quiz-components/xor-checkbox/xor-checkbox.component.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class XORComponent extends Component {
+    state = { checkbox1Checked: false, checkbox2Checked: false };
+
+    handleOnChange = event => {
+        const { id } = event.currentTarget;
+        this.setState({ [id + 'Checked']: !this.state[id + 'Checked'] });
+    };
+
+    render() {
+        const { checkbox1Checked, checkbox2Checked } = this.state;
+        const checkbox3Checked = checkbox1Checked ^ checkbox2Checked;
+
+        return (
+            <div style={{ margin: 150 }}>
+                <h2>Problem Statement:</h2>
+                <span>
+                    Render three checkboxes. User should be able to set the first and the second checkbox. Third
+                    checkbox should be an XOR (^) of the state of first and the second and should not be selectable.
+                </span>
+                <h3>Solution:</h3>
+                <hr />
+                <input type="checkbox" id="checkbox1" onChange={this.handleOnChange} checked={checkbox1Checked} />
+                <input type="checkbox" id="checkbox2" onChange={this.handleOnChange} checked={checkbox2Checked} />
+                <input type="checkbox" id="checkbox3" onChange={this.handleOnChange} checked={checkbox3Checked} />
+            </div>
+        );
+    }
+}
+
+export default XORComponent;
